c3f2859dba92ab0c4f2f2cdff5265a61fbe0afad
Jose Robson <111148505+JoseRobson2301@users.noreply.github.com>
2023-06-06 17:47:19 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmApplicationMain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcessar_Click(object sender, EventArgs e)
+        {
+           if(txtCod.Text.Trim() == "" || txtUsuario.Text.Trim() == "" || txtSenha.Text.Trim() == "")
+           {
+               MessageBox.Show("Peço desculpas, mas os campos fornecidos são inválidos. Por favor, forneça as informações corretas :(");
+
+               return;
+           }
+
+           FrmControleUser frm = new FrmControleUser();
+           frm.ShowDialog();
+        }
+    }
+}
---
+++ FrmControleUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmApplicationMain
+{
+    public partial class FrmControleUser : Form
+    {
+        public FrmControleUser()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
